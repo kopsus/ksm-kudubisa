8e7ea0b8f75c2638af6525445ca7843f218cd6d7
kopsus <emonsetio01@gmail.com>
2024-12-19 15:21:10 +0700

    fixing dashboard

--- src/components/dashboard/_global/Header/index.tsx
+++ src/components/dashboard/_global/Header/index.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import DropdownUser from "./DropdownUser";
-import Image from "next/image";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
@@ -52,15 +50,6 @@ const Header = (props: {
             </span>
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
-
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image
-              width={32}
-              height={32}
-              src={"/images/logo/logo-icon.svg"}
-              alt="Logo"
-            />
-          </Link>
         </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
