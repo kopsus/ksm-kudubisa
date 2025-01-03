7c21c1d93c8546588f364baf2ee82c4a80c74c2f
kopsus <emonsetio01@gmail.com>
2025-01-03 21:20:33 +0700

    update ci/cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,23 @@
-// import { Footer } from "@/components/_global/footer";
-// import { Header } from "@/components/_global/header";
-// import { Gallery } from "@/components/home/gallery";
-// import { HighlightHome } from "@/components/home/highlight";
-// import { ListSampah } from "@/components/home/listSampah";
-// import { PeduliSampah } from "@/components/home/peduliSampah";
-// import { SambutanKetua } from "@/components/_global/sambutanKetua";
-// import ButtonTrigger from "@/components/_global/buttonTrigger";
+import { Footer } from "@/components/_global/footer";
+import { Header } from "@/components/_global/header";
+import { Gallery } from "@/components/home/gallery";
+import { HighlightHome } from "@/components/home/highlight";
+import { ListSampah } from "@/components/home/listSampah";
+import { PeduliSampah } from "@/components/home/peduliSampah";
+import { SambutanKetua } from "@/components/_global/sambutanKetua";
+import ButtonTrigger from "@/components/_global/buttonTrigger";
 
 export default function Home() {
   return (
     <div className="max-w-screen-2xl mx-auto">
-      <p>test</p>
-      {/* <Header />
+      <Header />
       <HighlightHome />
       <PeduliSampah />
       <SambutanKetua />
       <ListSampah />
       <Gallery />
       <Footer />
-      <ButtonTrigger /> */}
+      <ButtonTrigger />
     </div>
   );
 }
