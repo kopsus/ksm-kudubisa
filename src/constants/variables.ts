c00774f961893c69b0d321f4b8156c2753ce38f2
kopsus <emonsetio01@gmail.com>
2025-01-04 11:23:09 +0700

    update pm2

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,8 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
-const imageURL = process.env.NEXT_PUBLIC_IMAGE;
+
 const roleId = process.env.NEXT_PUBLIC_ROLE_MASYARAKAT;
 
-export { prisma, baseURL, roleId, imageURL };
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const imageURL = process.env.NEXT_PUBLIC_IMAGE;
+
+// cloudflare
+const cloudflareURL = process.env.BASE_URL_CLOUDFLIRE;
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const secretAccessKey = process.env.SECRETE_ACCESS_KEY;
+
+export {
+  prisma,
+  baseURL,
+  roleId,
+  imageURL,
+  cloudflareURL,
+  accessKeyId,
+  secretAccessKey,
+};
